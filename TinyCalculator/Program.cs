38e03f81a13034b0cd727061f13a678a93ef3fb2
Pichelsberger Florian <F.Pichelsberger@htlvb.at>
2026-03-06 11:19:25 +0100

    first calculation

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("== Tiny Calculator ==");
+
+            int a = 5;
+            int b = 10;
+            Console.WriteLine($"{a}+{b}={a+b}");
         }
     }
 }
